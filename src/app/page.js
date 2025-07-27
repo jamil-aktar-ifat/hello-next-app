28c2bef9cd46e9faeba8f4ff6ec51ff154655a06
Jamil-Aktar-Ifat <jamilaktarifat@hotmail.com>
2025-07-27 23:08:56 +0100

    understanding limits of static sties with ISR

--- src/app/page.js
+++ src/app/page.js
@@ -3,14 +3,17 @@ import ProductCard from "./components/ProductCard";
 
 const HomePage = async () => {
   const res = await fetch("http://localhost:5001/products/", {
-    cache: "force-cache",
+    // cache: "force-cache",
+    next: {
+      revalidate: 5,
+    },
   });
   const products = await res.json();
   //   console.log(products);
   return (
     <div className="text-center">
       <h1 className="text-center text-2xl my-5">
-        Data Fetching, Caching, and Revalidating
+        Data Fetching, Caching, and Revalidating Jamil
       </h1>
       <div className="grid grid-cols-3 gap-8 w-[90%] mx-auto">
         {products.map((product) => (
