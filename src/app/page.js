ed655c27e2e484d29066eab177c394f16bd7b415
Jamil-Aktar-Ifat <jamilaktarifat@hotmail.com>
2025-07-31 14:02:07 +0100

    advanced data fetching with SSR

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ProductCard from "./components/ProductCard";
+import Link from "next/link";
 
 const HomePage = async () => {
   const res = await fetch("http://localhost:5001/products/", {
@@ -15,10 +16,18 @@ const HomePage = async () => {
       <h1 className="text-center text-2xl my-5">
         Data Fetching, Caching, and Revalidating Jamil
       </h1>
-      <div className="grid grid-cols-3 gap-8 w-[90%] mx-auto">
+      <div className="grid grid-cols-3 gap-8 w-[90%] mx-auto text-center">
         {products.map((product) => (
           <ProductCard key={product.id} product={product}></ProductCard>
         ))}
+      </div>
+      <div className="my-20">
+        <Link
+          href={`/products/`}
+          className="text-white bg-blue-500 py-2 px-4 hover:bg-blue-700"
+        >
+          See More
+        </Link>
       </div>
     </div>
   );
--- src/app/products/page.js
+++ src/app/products/page.js
@@ -1,11 +1,21 @@
+import ProductCard from "../components/ProductCard";
 
-
-const ProductPage = () => {
-    return (
-        <div>
-            <h2 className="text-2xl text-center pt-20">This is products page!</h2>
-        </div>
-    );
+const AllProductsPage = async () => {
+  const res = await fetch("http://localhost:5001/products", {
+    cache: "no-store",
+  });
+  const products = await res.json();
+  //   console.log(products);
+  return (
+    <div>
+      <h1 className="text-2xl text-center">All Products</h1>
+      <div className="grid grid-cols-3 gap-8 mx-auto my-10">
+        {products.map((product) => (
+          <ProductCard key={product.id} product={product}></ProductCard>
+        ))}
+      </div>
+    </div>
+  );
 };
 
-export default ProductPage;
+export default AllProductsPage;
